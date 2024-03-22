a1d0ecbb9a86d5789095d8a35ef3d2b833c8f035
Rohit <rohitk14458@gmail.com>
2024-03-22 18:25:52 +0530

    update img

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-import user1Img from './assets/user1.jpg'
-import user2Img from './assets/user2.jpeg'
-import "./App.css"
+import user1Img from "./assets/user1.jpeg";
+import user2Img from "./assets/user2.jpeg";
+import "./App.css";
 
 const SOCKET_URL = "http://192.168.0.103:3000";
 
 const App = () => {
   const [message, setMessage] = useState("");
-  const [myId,setMyId] = useState("");
+  const [myId, setMyId] = useState("");
   const [socket, setSocket] = useState(null);
   const [messageFromServer, setMessageFromServer] = useState([]);
 
@@ -26,16 +26,18 @@ const App = () => {
       console.log("Connnected !");
       setSocket(socket);
     });
-    
+
     socket.on("message", (newMessage) => {
-      console.log(`message from this client ${newMessage.id} to server: `, newMessage.message);
-      setMessageFromServer(prevMessages => [...prevMessages, newMessage]);
+      console.log(
+        `message from this client ${newMessage.id} to server: `,
+        newMessage.message
+      );
+      setMessageFromServer((prevMessages) => [...prevMessages, newMessage]);
     });
 
     socket.on("disconnect", () => {
       console.log("Disconnected from server");
     });
-
   }, []);
 
   useEffect(() => {
@@ -59,46 +61,76 @@ const App = () => {
     >
       <div className="chat-container">
         <div className="message-box">
-          {messageFromServer.map((message, index) => (
-            <div style={{
-              padding: '5px',
+          {messageFromServer.length != 0 ? (
+            messageFromServer.map((message, index) => (
+              <div
+                style={{
+                  padding: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: `${
+                    message.id == myId ? "flex-start" : "flex-end"
+                  }`,
+                  marginTop: "10px",
+                }}
+                className="message"
+                key={index}
+              >
+                <div
+                  className="chat-box"
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    borderRadius: "50%",
+                    backgroundColor: "#000",
+                    overflow: "hidden",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginRight: "10px",
+                  }}
+                >
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={`${message.id == myId ? user1Img : user2Img} `}
+                    alt="error"
+                  />
+                </div>
+                <p
+                  className="message-text"
+                  style={{
+                    background: `${message.id == myId ? "green" : "red"}`,
+                    display: "inline-block",
+                    padding: "2px 5px",
+                    borderRadius: "5px",
+                    color: `white`,
+                    fontFamily: "monospace",
+                    fontSize: "17px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {message.message}
+                </p>
+              </div>
+            ))
+          ) : (
+            <div className="" style={{
+              width: '100%',
+              height: '100%',
               display: 'flex',
               alignItems: 'center',
-              justifyContent:`${message.id == myId ? 'flex-start': 'flex-end'}`,
-              marginTop: '10px'
-            }} className="message" key={index}>
-              <div style={{
-                width: '50px',
-                height: '50px',
-                borderRadius: '50%',
-                backgroundColor: '#000',
-                overflow: 'hidden',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginRight: '10px'
-              }}>
-                <img style={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover'
-                }} src={`${message.id == myId ? user1Img : user2Img} `} alt="error" />
-              </div>
-              <p className="message-text" style={{
-                background: `${message.id == myId ? 'green' : 'red'}`,
-                display: 'inline-block',
-                padding: '2px 5px',
-                borderRadius: '5px',
-                color: `white`,
-                fontFamily:'monospace',
-                fontSize: '17px',
-                fontWeight: 'bold'
-              }}>{message.message}</p>
+              justifyContent: 'center'
+            }}>
+              <h1 className="">Hello world</h1>
             </div>
-          ))}
+          )}
         </div>
         <form
-        className="form"
+          className="form"
           style={{
             display: "flex",
             alignItems: "center",
