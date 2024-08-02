2de89f088ee23667ee04f4b5f55350fc1ed9cc3c
Rohit <rohitk14458@gmail.com>
2024-08-02 20:37:18 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './modules/onboarding/login/Login';
 import Register from './modules/onboarding/register/Register';
@@ -10,10 +10,17 @@ import Profile from './modules/onboarding/profile/Profile';
 function App() {
     return (
         <Routes>
-            <Route path='/' element={<h1>Hello world</h1>} />
+            <Route path='/' element={<div className='w-full h-screen flex flex-col gap-6 items-center justify-center'>
+                <h1 className='text-3xl'>Welcome to RohitChat</h1>
+                <div className='flex flex-row items-center gap-x-3'>
+                    <Link to={'/login'} className='px-3 py-2 rounded-md bg-blue-400 text-white'>Login</Link>
+                    <p>Or</p>
+                    <Link to={'/register'} className='px-3 py-2 rounded-md bg-blue-400 text-white'>Register</Link>
+                </div>
+            </div>} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/profile' element={<Profile />} />
             <Route path='/chats' element={<ChatHome />}>
                 <Route index element={
                     <div className='flex justify-center items-center gap-y-2 rounded w-full h-full'>
@@ -24,7 +31,7 @@ function App() {
                             <button className="px-3 py-2 rounded-md bg-blue-400 text-white text-sm">Send message</button>
                         </div>
                     </div>
-                }/>
+                } />
                 <Route path=':socketId' element={<ChatWithFriend />} />
             </Route>
         </Routes>
