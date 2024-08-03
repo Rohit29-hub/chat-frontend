40f5767f2f7e5a2a88bc9d7f3ac36b0001cf32cb
rohittttt29 <131178081+Rohit29-hub@users.noreply.github.com>
2024-08-03 16:52:30 +0530

    Update Login.tsx

--- src/modules/onboarding/login/Login.tsx
+++ src/modules/onboarding/login/Login.tsx
@@ -25,7 +25,7 @@ const Login = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const response = await fetch('http://localhost:8000/api/user/login', {
+            const response = await fetch('https://chat-backend-puxf.onrender.com/api/user/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
